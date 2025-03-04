a06cb64b24877c1b04d1723f213f665dc318a1a9
Edward911 <edikyurov72@gmail.com>
2025-03-04 22:41:13 +0300

    UpdateProjectAndTask

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -7,7 +7,7 @@ namespace WebProject.Controllers
     public class TaskController : Controller
     {
         private readonly ProjectDbContext _dbContext;
-        private readonly ILogger<TaskController> _logger;
+        private readonly ILogger<TaskController> _logger;   
 
         public TaskController(ProjectDbContext dbContext, ILogger<TaskController> logger)
         {
@@ -26,6 +26,8 @@ namespace WebProject.Controllers
             var task = new MyTask(); 
             return View(task); 
         }
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Add(MyTask task)
@@ -55,6 +57,71 @@ namespace WebProject.Controllers
 
             return View();
         }
+        [HttpPost]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = _dbContext.Tasks.Find(id);
+            if(task == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Tasks.Remove(task);
+            _dbContext.SaveChanges();
+            TempData["SuccessMessage"] = "Задача успешно удалена";
+            return RedirectToAction(nameof(task));
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(MyTask myTask)
+        {
+            if(ModelState.IsValid)
+            {
+                if(!_dbContext.Tasks.Any(p=>p.Id == myTask.Id))
+                {
+                    return NotFound();
+                }
+                _dbContext.Tasks.Update(myTask);
+                _dbContext.SaveChanges();
+                TempData["SuccessMessage"] = "Задача успешно обновлена";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(myTask);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var tas = _dbContext.Tasks.Find(id);
+
+            if (tas == null)
+            {
+                return NotFound();
+            }
+            return View(tas);
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null || id ==0)
+            {
+                return NotFound("ID задачи не найден");
+            }
+            var task = _dbContext.Tasks.Find(id);
+            if(task == null)
+            {
+                return NotFound("задача не найдена");
+            }
+            return View(task);
+        }
+
 
         [HttpGet]
         public IActionResult Index()
